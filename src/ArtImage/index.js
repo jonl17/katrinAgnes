41730842e0a18d189f15ce9e0e8ddc76cdeb4aca
Jón <Lorange>
2019-05-24 13:56:47 +0000

    more progress hun

--- src/ArtImage/index.js
+++ src/ArtImage/index.js
@@ -6,71 +6,115 @@ import { Stickyroll } from "@stickyroll/stickyroll"
 import Img from "gatsby-image"
 
 class ArtImage extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      images: this.props.images.allFile.edges,
-      maxScroll: window.innerHeight - 100,
-      posX: 0,
-      index: 0,
+    constructor(props) {
+        super(props)
+        this.state = {
+            images: this.props.images.allFile.edges,
+            maxScroll: window.innerHeight - 100,
+            posX: 0,
+            index: 0,
+            displayedImages: [], // images to stick
+            displayedImagesCords: [], // left and top pos of these images
+        }
+        this.drawCanvas = this.drawCanvas.bind(this)
     }
-  }
 
-  render() {
-    return (
-      <>
-        <Img
-          style={{
-            height: "100%",
-            width: "100%",
-            maxHeight: 400,
-            maxWidth: 400,
-            position: "fixed",
-            top: window.innerHeight,
-            left: `${this.state.posX}%`,
-            transform: `translateY(-${this.state.maxScroll}px)`,
-          }}
-          className="test-img"
-          fluid={this.state.images[this.state.index].node.childImageSharp.fluid}
-        />
-        <Stickyroll pages={this.state.images} factor={5}>
-          {({ page, pageIndex, pages, progress }) => {
-            let y = progress * 1000
-            let x = page * 10
-            if (this.state.posX !== x) {
-              this.setState({
-                posX: x,
-              })
-            }
-            if (y >= this.state.maxScroll) {
-              y = this.state.maxScroll
-              if (pageIndex !== this.state.index) {
-                this.setState({
-                  index: pageIndex,
-                })
-              }
-              if (this.state.posX !== x) {
-                this.setState({
-                  posX: x,
-                })
-              }
-            }
-            return (
-              <>
-                <Image
-                  x={x}
-                  y={y}
-                  fluid={
-                    this.state.images[pageIndex].node.childImageSharp.fluid
-                  }
-                />
-              </>
-            )
-          }}
-        </Stickyroll>
-      </>
-    )
-  }
+    appendDisplayedImages(image) {
+        const list = [...this.state.displayedImages, image]
+        this.setState({
+            displayedImages: list,
+        })
+    }
+    appendCords(cords) {
+        const list = [...this.state.displayedImagesCords, cords]
+        this.setState({
+            displayedImagesCords: list,
+        })
+        console.log(list)
+    }
+    drawCanvas() {
+        /* draws images to the screen that shall be displayed */
+        console.log(this.state.displayedImagesCords)
+        return this.state.displayedImages.map((item, index) => (
+            <Img
+                key={index}
+                style={{
+                    height: "100%",
+                    width: "100%",
+                    maxHeight: 400,
+                    maxWidth: 400,
+                    position: "fixed",
+                    top: window.innerHeight,
+                    left: `${this.state.displayedImagesCords[index].x}%`,
+                    transform: `translateY(-${
+                        this.state.displayedImagesCords[index].y
+                    }px)`,
+                }}
+                className="test-img"
+                fluid={item}
+            />
+        ))
+    }
+
+    render() {
+        return (
+            <>
+                {this.drawCanvas() /* this function is quite fantastic, displays images that should stick */}
+                <Stickyroll pages={this.state.images} factor={5}>
+                    {({ page, pageIndex, pages, progress }) => {
+                        var y = progress * 1000
+                        var x = page * 10
+                        if (this.state.posX !== x) {
+                            this.setState({
+                                posX: x,
+                            })
+                        }
+                        if (y >= this.state.maxScroll) {
+                            y = this.state.maxScroll
+                            /* append to the image array of displayed ones */
+                            if (
+                                this.state.displayedImages[pageIndex] !==
+                                this.state.images[pageIndex].node
+                                    .childImageSharp.fluid
+                            ) {
+                                this.appendDisplayedImages(
+                                    this.state.images[pageIndex].node
+                                        .childImageSharp.fluid
+                                )
+                                /* Here we append the cords of the images that need to get sticked, only ones for each image */
+                                this.appendCords({ x: x, y: y })
+                                /* ----- */
+                            }
+
+                            if (pageIndex !== this.state.index) {
+                                this.setState({
+                                    index: pageIndex,
+                                })
+                            }
+                            if (this.state.posX !== x) {
+                                this.setState({
+                                    posX: x,
+                                })
+                            }
+                        }
+
+                        return (
+                            <>
+                                <Image
+                                    x={x}
+                                    y={y}
+                                    fluid={
+                                        this.state.images[pageIndex].node
+                                            .childImageSharp.fluid
+                                    }
+                                />
+                            </>
+                        )
+                    }}
+                </Stickyroll>
+            </>
+        )
+    }
 }
 
 export default ArtImage
